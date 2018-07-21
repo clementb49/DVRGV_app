c92821f1130b595bc86c3f427e0ba4bba8baa5da
clementb49 <clement.boussiron@gmail.com>
2018-07-21 11:11:37 +0200

    Initial Commit

---
+++ DVRGV_app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DVRGV_app
+//
+//  Created by Clément BOUSSIRON on 21/07/2018.
+//  Copyright © 2018 Clément BOUSSIRON. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
