6d0c2d06c5441fe09622e8b250896adc467c8a99
clementb49 <clement.boussiron@gmail.com>
2018-07-22 11:55:14 +0200

    added enumeration for API verb

---
+++ DVRGV_app/API/APIVerb.swift
@@ -0,0 +1,16 @@
+//
+//  APIVerb.swift
+//  DVRGV_app
+//
+//  Created by Clément BOUSSIRON on 22/07/2018.
+//  Copyright © 2018 Clément BOUSSIRON. All rights reserved.
+//
+
+import Foundation
+// http verb of the API
+enum APIVerb:String {
+	case GET
+	case POST
+	case PUT
+	case DELETE
+}
