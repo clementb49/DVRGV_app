a4a6d20e5d8324bb1fea42b36fab354345216859
clementb49 <clement.boussiron@gmail.com>
2018-08-08 23:16:29 +0200

    add a new view controller named detail podcast create segue and create connexion between the view controller and the code

---
+++ DVRGV_app/podcast/DetailPodcastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailPodcastViewController.swift
+//  DVRGV_app
+//
+//  Created by Clément BOUSSIRON on 08/08/2018.
+//  Copyright © 2018 Clément BOUSSIRON. All rights reserved.
+//
+
+import UIKit
+
+class DetailPodcastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
