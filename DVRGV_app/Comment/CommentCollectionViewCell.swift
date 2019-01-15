c0734afaacd49c9f7302fa052e0765ea637601f5
clementb49 <clement.boussiron@gmail.com>
2019-01-15 10:37:28 +0100

    change table view to collection view

---
+++ DVRGV_app/Comment/CommentCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CommentCollectionViewCell.swift
+//  DVRGV_app
+//
+//  Created by clément boussiron on 15/01/2019.
+//  Copyright © 2019 Clément BOUSSIRON. All rights reserved.
+//
+
+import UIKit
+
+class CommentCollectionViewCell: UICollectionViewCell {
+    
+}
