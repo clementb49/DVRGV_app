25a455dedcac6cbbcb4ca0ce70d78c4eddf6ef65
clementb49 <clement.boussiron@gmail.com>
2019-01-28 09:26:07 +0100

    create View to add comment

---
+++ DVRGV_app/Comment/AddCommentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddCommentViewController.swift
+//  DVRGV_app
+//
+//  Created by clément boussiron on 28/01/2019.
+//  Copyright © 2019 Clément BOUSSIRON. All rights reserved.
+//
+
+import UIKit
+
+class AddCommentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
